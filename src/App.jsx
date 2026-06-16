88f7b1572440ef3fbc7b97eff691eed5d94b2429
VivekKumarRay22 <vivekkumarray855@gmail.com>
2026-06-16 18:54:44 +0530

    added draggable and resizeable component using react-rnd

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import "./app.scss"
 import Dock from './components/Dock'
 import Nav from './components/Nav'
+import { Rnd } from 'react-rnd'
+import MacWindows from './components/windows/MacWindows'
 
 const App = () => {
   return (
       <main>
         <Nav />
         <Dock />
+
+       <MacWindows />
       </main>
   )
 }
