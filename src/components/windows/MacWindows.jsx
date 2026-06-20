1173a5b41ecb801bfd5540493b699096969de137
VivekKumarRay22 <vivekkumarray855@gmail.com>
2026-06-20 14:37:03 +0530

    feat: add fullscreen window functionality

--- src/components/windows/MacWindows.jsx
+++ src/components/windows/MacWindows.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Rnd } from "react-rnd";
 import "./windows.scss"
 
 const MacWindows = ({ children, width = '40vw', height = '40vh', windowName, windowsState, setWindowsState }) => {
+    const [fullscreen, setFullscreen] = useState(false);
+
     return (
         <Rnd
-            default={{
-                x: 50,
-                y: 50,
-                width: width,
-                height: height
-            }} >
+            size={{
+                width: fullscreen ? "100vw" : width,
+                height: fullscreen ? "100vh" : height
+            }}
+            position={{
+                x: fullscreen ? 0 : 50,
+                y: fullscreen ? 0 : 50
+            }}
+        >
             <div className="windows">
                 <div className="nav">
                     <div className="dots">
                         <div onClick={() => { setWindowsState(state => ({ ...state, [windowName]: false })) }}
                             className="dot red"></div>
                         <div className="dot yellow"></div>
-                        <div className="dot green"></div>
+                        <div onClick={() => { setFullscreen(prev => !prev) }} className="dot green"></div>
                     </div>
                     <div className="title"><p> vivekkumarray - zxh</p></div>
                 </div>
